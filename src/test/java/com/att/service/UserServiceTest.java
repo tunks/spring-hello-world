326f7ce8473066ab871574166a5c389ad18b5f62
ebrimatunkara <ebrimatunkara@gmail.com>
2016-07-17 01:15:57 -0400

    updated test unit

--- src/test/java/com/att/service/UserServiceTest.java
+++ src/test/java/com/att/service/UserServiceTest.java
@@ -70,7 +70,7 @@ public class UserServiceTest {
      */
     @Test
     public void testAddUser() {
-        User user = new User("3001","User20111");
+        User user = new User("3001","Use30111");
         User result = userService.addUser(user);
         assertEquals(user, result);
     }
